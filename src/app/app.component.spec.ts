177bf248378733147431773db1d1340df8345ab2
Michael Small <chickenhat22@gmail.com>
2024-06-02 10:47:49 -0500

    tests: add until tests for form signal util

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,114 @@
-import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { AsyncPipe, JsonPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import {allEventsObservable, allEventsSignal} from "./form-events";
 
-describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+function flattenJsonPipeFormatting(str: string | null) {
+  return str?.replace(/\s/g, '');
+}
+describe('Form Events', () => {
+  it('returns a signal with the initial value, status, pristine, and touched values of a form', async () => {
+    const fixture: ComponentFixture<FormEventsComponent> =
+      TestBed.configureTestingModule({
+        imports: [FormEventsComponent],
+      }).createComponent(FormEventsComponent);
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'form-event-signals-article' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('form-event-signals-article');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, form-event-signals-article');
+
+    const signalVals: HTMLElement = fixture.debugElement.query(
+      By.css('[data-testid="signal-values"]'),
+    ).nativeElement;
+
+    expect(flattenJsonPipeFormatting(signalVals.textContent)).toBe(
+      flattenJsonPipeFormatting(`            {
+            "value": {
+                "firstName": "",
+                "lastName": ""
+            },
+            "status": "INVALID",
+            "touched": false,
+            "pristine": true
+            }`),
+    );
   });
+
+  it('returns a signal with the value, status, pristine, and touched values of a form after it has been interacted with', async () => {
+    const fixture: ComponentFixture<FormEventsComponent> =
+      TestBed.configureTestingModule({
+        imports: [FormEventsComponent],
+      }).createComponent(FormEventsComponent);
+
+    fixture.detectChanges();
+
+    const signalVals: HTMLElement = fixture.debugElement.query(
+      By.css('[data-testid="signal-values"]'),
+    ).nativeElement;
+
+    const formFirstNameInput: HTMLInputElement =
+      fixture.debugElement.nativeElement
+        .querySelector('#test-form')
+        .querySelectorAll('input')[0];
+
+    formFirstNameInput.focus();
+    formFirstNameInput.value = 'Jerry';
+    formFirstNameInput.dispatchEvent(new Event('input'));
+    formFirstNameInput.blur();
+
+    fixture.detectChanges();
+
+    expect(flattenJsonPipeFormatting(signalVals.textContent)).toBe(
+      flattenJsonPipeFormatting(`{
+            "value": {
+                "firstName": "Jerry",
+                "lastName": ""
+            },
+            "status": "VALID",
+            "touched": true,
+            "pristine": false
+            }`),
+    );
+  });
+
+  // TODO - observables
 });
+
+@Component({
+  standalone: true,
+  imports: [ReactiveFormsModule, JsonPipe, AsyncPipe],
+  template: `
+		<form [formGroup]="form" id="test-form">
+			<label for="firstName">Name</label>
+			<input
+				data-testid="firstName"
+				formControlName="firstName"
+				name="firstName"
+			/>
+			<label for="lastName">Name</label>
+			<input
+				data-testid="lastName"
+				formControlName="lastName"
+				name="lastName"
+			/>
+			<br />
+			<button type="reset">Reset</button>
+		</form>
+		<pre data-testid="signal-values">{{ $form() | json }}</pre>
+		<pre data-testid="observable-values">{{ form$ | async | json }}</pre>
+	`,
+})
+export default class FormEventsComponent {
+  fb = inject(NonNullableFormBuilder);
+  form = this.fb.group({
+    firstName: this.fb.control('', Validators.required),
+    lastName: this.fb.control(''),
+  });
+
+  form$ = allEventsObservable(this.form);
+  $form = allEventsSignal(this.form);
+}
