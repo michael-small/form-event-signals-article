1d99c26989fc6b586222151d2ec3ffea3eccb737
Michael Small <chickenhat22@gmail.com>
2024-06-11 08:24:09 -0500

    test: add tests for v18 utils

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -33,7 +33,42 @@ describe('Form Events', () => {
             },
             "status": "INVALID",
             "touched": false,
-            "pristine": true
+            "pristine": true,
+            "valid": false,
+            "invalid": true,
+            "pending": false,
+            "dirty": false,
+            "untouched": true
+            }`),
+    );
+  });
+
+  it('returns an observable with the initial value, status, pristine, and touched values of a form', async () => {
+    const fixture: ComponentFixture<FormEventsComponent> =
+      TestBed.configureTestingModule({
+        imports: [FormEventsComponent],
+      }).createComponent(FormEventsComponent);
+
+    fixture.detectChanges();
+
+    const signalVals: HTMLElement = fixture.debugElement.query(
+      By.css('[data-testid="observable-values"]'),
+    ).nativeElement;
+
+    expect(flattenJsonPipeFormatting(signalVals.textContent)).toBe(
+      flattenJsonPipeFormatting(`            {
+            "value": {
+                "firstName": "",
+                "lastName": ""
+            },
+            "status": "INVALID",
+            "touched": false,
+            "pristine": true,
+            "valid": false,
+            "invalid": true,
+            "pending": false,
+            "dirty": false,
+            "untouched": true
             }`),
     );
   });
@@ -70,37 +105,82 @@ describe('Form Events', () => {
             },
             "status": "VALID",
             "touched": true,
-            "pristine": false
+            "pristine": false,
+            "valid": true,
+            "invalid": false,
+            "pending": false,
+            "dirty": true,
+            "untouched": false
             }`),
     );
   });
 
-  // TODO - observables
+  it('returns a signal with the value, status, pristine, and touched values of a form after it has been interacted with', async () => {
+    const fixture: ComponentFixture<FormEventsComponent> =
+      TestBed.configureTestingModule({
+        imports: [FormEventsComponent],
+      }).createComponent(FormEventsComponent);
+
+    fixture.detectChanges();
+
+    const signalVals: HTMLElement = fixture.debugElement.query(
+      By.css('[data-testid="observable-values"]'),
+    ).nativeElement;
+
+    const formFirstNameInput: HTMLInputElement =
+      fixture.debugElement.nativeElement
+        .querySelector('#test-form')
+        .querySelectorAll('input')[0];
+
+    formFirstNameInput.focus();
+    formFirstNameInput.value = 'Jerry';
+    formFirstNameInput.dispatchEvent(new Event('input'));
+    formFirstNameInput.blur();
+
+    fixture.detectChanges();
+
+    expect(flattenJsonPipeFormatting(signalVals.textContent)).toBe(
+      flattenJsonPipeFormatting(`{
+            "value": {
+                "firstName": "Jerry",
+                "lastName": ""
+            },
+            "status": "VALID",
+            "touched": true,
+            "pristine": false,
+            "valid": true,
+            "invalid": false,
+            "pending": false,
+            "dirty": true,
+            "untouched": false
+            }`),
+    );
+  });
 });
 
 @Component({
   standalone: true,
   imports: [ReactiveFormsModule, JsonPipe, AsyncPipe],
   template: `
-		<form [formGroup]="form" id="test-form">
-			<label for="firstName">Name</label>
-			<input
-				data-testid="firstName"
-				formControlName="firstName"
-				name="firstName"
-			/>
-			<label for="lastName">Name</label>
-			<input
-				data-testid="lastName"
-				formControlName="lastName"
-				name="lastName"
-			/>
-			<br />
-			<button type="reset">Reset</button>
-		</form>
-		<pre data-testid="signal-values">{{ $form() | json }}</pre>
-		<pre data-testid="observable-values">{{ form$ | async | json }}</pre>
-	`,
+    <form [formGroup]="form" id="test-form">
+      <label for="firstName">First Name</label>
+      <input
+        data-testid="firstName"
+        formControlName="firstName"
+        name="firstName"
+      />
+      <label for="lastName">Last Name</label>
+      <input
+        data-testid="lastName"
+        formControlName="lastName"
+        name="lastName"
+      />
+      <br />
+      <button type="reset">Reset</button>
+    </form>
+    <pre data-testid="signal-values">{{ $form() | json }}</pre>
+    <pre data-testid="observable-values">{{ form$ | async | json }}</pre>
+  `,
 })
 export default class FormEventsComponent {
   fb = inject(NonNullableFormBuilder);
